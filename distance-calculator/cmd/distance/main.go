1e00146d545260543fae6746b577d28ecb130caa
saneaizog <pavlusha740@gmail.com>
2025-04-22 15:08:04 +0300

    Обновил образ контейнера до версии v1.1.0 и добавил поддержку конфигурации через ConfigMap. Вывод формулы расчета расстояния теперь отображается в консоли.

--- distance-calculator/cmd/distance/main.go
+++ distance-calculator/cmd/distance/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/saneaizog/kratix/distance-calculator/internal/calculator"
 )
 
 func main() {
+	formula := os.Getenv("FORMULA")
+	fmt.Printf("Используемая формула: %s\n", formula)
 	var x1, y1, x2, y2 float64
 	fmt.Println("Введите координаты двух точек (x1 y1 x2 y2):")
 	_, err := fmt.Scan(&x1, &y1, &x2, &y2)
